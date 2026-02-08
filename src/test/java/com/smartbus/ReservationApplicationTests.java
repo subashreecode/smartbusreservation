50a1638e9aa17c8d86785392478f1151981ae32a
subashreecode <rsubashree21@outlook.com>
2026-02-08 21:52:09 +0000

    project setup with spring boot + dependencies

---
+++ src/test/java/com/smartbus/ReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
